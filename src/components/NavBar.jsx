f52e35afb4aeb46f06ca0e856f4fb4817abcf97f
Berlin <evan1234.ek@gmail.com>
2024-02-07 02:36:47 +0600

    Refactor App.jsx and add colorModeSwitch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} alt="logo" boxSize="60px" borderRadius="full" />
-      <Text>Online Judge Hunt</Text>
+
+      <ColorModeSwitch />
     </HStack>
   );
 };
