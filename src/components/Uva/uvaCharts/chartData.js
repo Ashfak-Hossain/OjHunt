0b7caf7167e6e9048998455d6e475a366fb1dc1c
Berlin <evan1234.ek@gmail.com>
2024-02-10 21:41:00 +0600

    Refactor calculateVerdictData function and add color mapping for verdicts

--- src/components/Uva/uvaCharts/chartData.js
+++ src/components/Uva/uvaCharts/chartData.js
@@ -1,50 +1,29 @@
 import { mapVerdictToLabel } from '../uvaTable/userSubmissionTable/mapUvaVerdict';
 
-const mapVerdicts = (submissionProblems) => {
+const calculateVerdictData = (submissionProblems) => {
   const verdictCounts = new Map();
 
   submissionProblems.forEach((problem) => {
     const verdict = mapVerdictToLabel(problem[2]);
-    if (verdictCounts.has(verdict)) {
-      verdictCounts.set(verdict, verdictCounts.get(verdict) + 1);
-    } else {
-      verdictCounts.set(verdict, 1);
-    }
+    verdictCounts.set(verdict, (verdictCounts.get(verdict) || 0) + 1);
   });
 
-  return verdictCounts;
-};
+  const labels = [...verdictCounts.keys()];
+  const data = [...verdictCounts.values()];
 
-const calculateVerdictData = (submissionProblems) => {
-  const labels = [...mapVerdicts(submissionProblems).keys()];
-  const data = [...mapVerdicts(submissionProblems).values()];
+  const percentages = data.map((count) =>
+    ((count / submissionProblems.length) * 100).toFixed(0)
+  );
 
-  const percentages = data.map((count) => {
-    return ((count / submissionProblems.length) * 100).toFixed(0);
-  });
+  const { backgroundColors, borderColors } = getColorsForVerdicts(labels);
 
   return {
     labels: labels,
     datasets: [
       {
-        // label: 'Verdict Rate',
         data: data,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
         borderWidth: 1,
         hoverOffset: 5,
       },
@@ -53,47 +32,50 @@ const calculateVerdictData = (submissionProblems) => {
   };
 };
 
-// const options = {
-//   layout: {
-//     padding: {
-//       left: 0,
-//       right: 0,
-//       top: 0,
-//       bottom: 0,
-//     },
-//   },
-//   plugins: {
-//     legend: {
-//       display: true,
-//       position: 'right',
-//       labels: {
-//         padding: 20,
-//         marginX: 10,
-//         generateLabels: function (chart) {
-//           const data = chart.data;
-//           if (data.labels.length && data.datasets.length) {
-//             return data.labels.map(function (label, i) {
-//               const value = data.datasets[0].data[i];
-//               const total = data.datasets[0].data.reduce(
-//                 (acc, val) => acc + val,
-//                 0
-//               );
-//               const percentage = ((value / total) * 100).toFixed(0) + '%';
-//               return {
-//                 text: `${label} ${value} (${percentage})`,
-//                 fillStyle: data.datasets[0].backgroundColor[i],
-//                 hidden:
-//                   isNaN(data.datasets[0].data[i]) ||
-//                   data.datasets[0].data[i] <= 0,
-//                 index: i,
-//               };
-//             });
-//           }
-//           return [];
-//         },
-//       },
-//     },
-//   },
-// };
+const getColorsForVerdicts = (verdicts) => {
+  const backgroundColors = [];
+  const borderColors = [];
+
+  verdicts.forEach((verdict) => {
+    const color = getColorForVerdict(verdict);
+    backgroundColors.push(color);
+    borderColors.push(getDeeperColor(color));
+  });
+
+  return { backgroundColors, borderColors };
+};
+
+const getColorForVerdict = (verdict) => {
+  switch (verdict) {
+    case '- In queue -':
+    case 'SubmissionErr':
+    case "Can't be judged":
+      return 'rgba(0, 0, 0, 0.3)';
+    case 'Compile error':
+      return 'rgba(170, 170, 0, 0.3)';
+    case 'Runtime error':
+      return 'rgba(0, 170, 170, 0.3)';
+    case 'Output limit':
+      return 'rgba(0, 0, 102, 0.3)';
+    case 'Time limit':
+      return 'rgba(54, 162, 235, 0.2)';
+    case 'Memory limit':
+      return 'rgba(153, 102, 255, 0.2)';
+    case 'Wrong answer':
+      return 'rgba(255, 99, 132, 0.2)';
+    case 'PresentationE':
+      return 'rgba(102, 102, 0, 0.3)';
+    case 'Accepted':
+      return 'rgba(75, 192, 192, 0.2)';
+    default:
+      return 'rgba(0, 0, 0, 0.3)';
+  }
+};
+
+const getDeeperColor = (color) => {
+  const rgbaArray = color.replace(/[^\d,]/g, '').split(',');
+  const alpha = Math.min(parseFloat(rgbaArray[3]) + 0.8, 1.0);
+  return `rgba(${rgbaArray[0]}, ${rgbaArray[1]}, ${rgbaArray[2]}, ${alpha})`;
+};
 
 export { calculateVerdictData };
